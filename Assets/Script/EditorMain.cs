123104f4fd4af9be1ded6631c8e3706e8c7386e0
织法 <hataksumo@163.com>
2019-02-20 18:53:31 +0800

    上传Excel操作文件

---
+++ Assets/Script/EditorMain.cs
@@ -0,0 +1,73 @@
+﻿using LuaInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class EditorMain : LuaClient, IDisposable
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    new void Reset()
+    {
+        Awake();
+    }
+
+
+    public void test()
+    {
+        LuaFunction fun = luaState.GetFunction("test");
+        if (fun == null)
+        {
+            ZFDebug.Error("can't find the funtion named output_excel");
+        }
+        fun.Call();
+        fun.Dispose();
+        fun = null;
+    }
+
+
+    public void output_excel()
+    {
+        LuaFunction fun = luaState.GetFunction("output_excel");
+        if (fun == null)
+        {
+            ZFDebug.Error("can't find the funtion named output_excel");
+        }
+        fun.Call();
+        fun.Dispose();
+        fun = null;
+    }
+    public void init()
+    {
+        Instance = this;
+        InitLoader();
+        luaState = new LuaState();
+        OpenLibs();
+        luaState.LuaSetTop(0);
+        Bind();
+        luaState.Start();
+        luaState.DoFile("EditorMain.lua");
+        levelLoaded = luaState.GetFunction("OnLevelWasLoaded");
+    }
+    public void reload()
+    {
+        luaState.DoFile("EditorMain.lua");
+    }
+
+    public void Dispose()
+    {
+        luaState.Dispose();
+    }
+}
---
+++ Assets/Script/ExcelHandle/Editor/GameMenu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+
+class GameMenu
+{
+    [MenuItem("Custom/doMain")]
+    public static void doMain()
+    {
+        var ins = EditorMain.GetInstence();
+        ins.test();
+    }
+}
